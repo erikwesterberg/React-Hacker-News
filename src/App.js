cb172f32fe912026aa2ea773c1656691e40b035a
erka¨git config --global user.name erkagit config --global user.name erka¨ <erik.westerberg@hotmail.se>
2019-10-13 10:50:42 +0200

    refactors app component, and changes the import of it in idex because its no longer exportet like default

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
 import React from 'react';
-import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      Yoooo new App!
-    </div>
-  );
-}
-
-export default App;
+export const App = () => <p>Hello</p> 
