b3e2e3ec0a8c075cdd0e5a38f5447310847e1338
erka¨git config --global user.name erkagit config --global user.name erka¨ <erik.westerberg@hotmail.se>
2019-10-13 12:36:19 +0200

    refactors the response from getstyor api req to send the data to a new created function that destructur choosen data

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getStoryIds, getStory } from "../services/hnApi";
+import { getStoryIds } from "../services/hnApi";
 import { Story } from "../components/Story";
 
 export const StoriesContainer = () => {
