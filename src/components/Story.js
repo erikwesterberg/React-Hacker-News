d3dd95d8729e44c2a52b3062e562de5163ad6934
erka¨git config --global user.name erkagit config --global user.name erka¨ <erik.westerberg@hotmail.se>
2019-10-13 11:53:31 +0200

    scaffolds Story component, imports useeffect and needed code base, does nothing yet

---
+++ src/components/Story.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { getStory } from "../services/hnApi";
+
+export const Story = () => {
+  useEffect(() => {
+    console.log("yeupp")
+  }, [])
+  return (
+
+  )
+}
